14992242a026d46ce2b7d342385b8c8ed7746db6
7vvks <45107550+7vvks@users.noreply.github.com>
2018-12-20 16:12:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("اكتب اي شي",{type: 'WATCHING'})
+   client.user.setActivity("Magic Song",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -37,7 +37,7 @@ const queue = new Map();
  
  
  
-var prefix = "البرفكس" 
+var prefix = "2" 
 client.on('message', async msg => {
     if (msg.author.bot) return undefined;
    
@@ -273,7 +273,9 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
    }
    });
  
- 
+ client.on('ready', () => {
+    client.channels.get("523525677165576212").join(); 
+    })
  
  
 client.login(process.env.BOT_TOKEN);
